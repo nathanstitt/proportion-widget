950e41950a65a47fbeb2c534b06ef60197a748e1
Nathan Stitt <nathan@stitt.org>
2010-06-23 11:51:15 -0500

    added util scripts

--- copy2.sh
+++ copy2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-cp -v public/javascripts/wonder_table.js $1/public/javascripts/
-cp -v public/stylesheets/wonder_table.css $1/public/stylesheets/
-cp -v public/images/icons/*.gif $1/public/images/icons/
+cp -v public/javascripts/proportion.js $1/public/javascripts/
+cp -v public/stylesheets/proportion.css $1/public/stylesheets/
+
 
--- copyFrom.sh
+++ copyFrom.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-cp -v $1/public/javascripts/wonder_table.js public/javascripts/
-cp -v $1/public/stylesheets/wonder_table.css public/stylesheets/
+cp -v $1/public/javascripts/proportion.js public/javascripts/
+cp -v $1/public/stylesheets/proportion.css public/stylesheets/
 
 
