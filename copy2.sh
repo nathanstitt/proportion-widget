bb1637850da414b6ee48b6cc090721ea1c5b70f4
Nathan Stitt <nathan@stitt.org>
2010-06-23 11:31:29 -0500

    added util scripts

---
+++ copy2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+cp -v public/javascripts/wonder_table.js $1/public/javascripts/
+cp -v public/stylesheets/wonder_table.css $1/public/stylesheets/
+cp -v public/images/icons/*.gif $1/public/images/icons/
+
